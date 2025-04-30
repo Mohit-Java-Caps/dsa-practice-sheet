5130a0f3d5cd00ca1a985642475297e123910c84
Mohit-Java-Caps <mohit02012001@gmail.com>
2025-04-30 11:54:23 +0530

    1295. Find Numbers with Even Number of Digits(Alternate Way)

---
+++ DailyLeetcodeProblems/findNumbersAlternateWay.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findNumbersAlternateWay(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if ((num >= 10 && num <= 99) ||
+                (num >= 1000 && num <= 9999) ||
+                (num >= 100000 && num <= 999999)) {
+                count++;
+            }
+        }
+        return count;
+             
+    }
+}
